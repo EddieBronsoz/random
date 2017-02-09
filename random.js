b77dc93a5421bf349891d774348bb805194c5497
Eddie <edspikester11@gmail.com>
2017-02-09 10:40:39 -0800

    halfway

--- random.js
+++ random.js
@@ -5,7 +5,9 @@ var namespace = "http://www.w3.org/2000/svg"
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createFirstScene() {
-    
+makeCircle(100, 50, 25, "cyan", 0.25)
+makeEllipse(100, 75, 10, 25, "red", 0.25)
+   
 }
 
 
@@ -32,17 +34,21 @@ function createThirdScene() {
 // This function is called whenever you press the "Go!" button.
 function createRandomScene() {
     // Generate a random number between 0 and 1, and store it in a variable.
-    
+    var myNumber= Math.random()
     // If the number is less than 0.33, call the function to create your first scene.
-    
-    
+    if(myNumber < 0.33) {
+        createFirstScene()
+        }
     
     // Else, if the number is less than 0.67, call the function to create your second scene.
-    
+    else if(myNumber < 0.67){
+     createSecondScene()  }
     
     
     // Else, call the function to create your third scene.
-    
+    else {
+        createThirdScene()
+    }
     
     
 }
